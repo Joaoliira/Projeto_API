feacd20c659d584679c0706dbdf4dae02b530c2b
joao <joaovitor.almeiidalira@gmail.com>
2026-08-12 17:18:00 -0300

    Iniciando Projeto

---
+++ src/main/java/com/example/Financias_API/FinanciasApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Financias_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanciasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanciasApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Financias_API/FinanciasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Financias_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanciasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
